86eaa4ea18d249150d18c7cfd4e9e820e028fbbf
Ryan Henning <ryan.henning@gmail.com>
2019-08-13 20:36:51 -0500

    Completing leetcode exercise 104

---
+++ leetcode/104_max-depth-binary-tree.cs
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    
+    public int MaxDepth(TreeNode root) {
+        
+        return Traverse(root, 1);
+    }
+    
+    public int Traverse(TreeNode currentNode, int currentDepth)
+    {
+        if (currentNode == null)
+        {
+            return 0;
+        }
+        
+        return Max(Traverse(currentNode.left, currentDepth),                   Traverse(currentNode.right, currentDepth)) + 1;
+        
+    }
+    
+    public int Max(int first, int second)
+    {
+        if (first >= second)
+        {
+            return first;
+        }
+        else
+        {
+            return second;
+        }
+    }
+}
